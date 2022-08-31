c5d7c3be8ba48a3a1ede41c889e8e3f5fe92e93f
Yiwan Zhou <mail@yiwan.me>
2022-09-01 01:44:05 +0800

    init

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from 'solid-start'
+import Counter from '~/components/Counter'
+
+export default function Home() {
+  return (
+    <main>
+      <Title>/</Title>
+      <Counter />
+    </main>
+  )
+}
